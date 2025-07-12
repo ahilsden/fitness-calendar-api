ac46e3af546ad9a6f50e7e29f479b1fe747c1481
Alyn Hilsden <=>
2025-07-12 12:21:21 +0100

    Adding StravaController to check all is well with Docker environment

---
+++ app/Http/Controllers/StravaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class StravaController extends Controller
+{
+    public function redirectToStrava()
+    {
+        return "all ok...";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\StravaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(
+    '/strava/auth',
+    [StravaController::class, 'redirectToStrava']
+)->name('web.strava.redirectToStrava');
