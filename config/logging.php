6480b52b69fbb0ca1c78c9cdf4d9503cea918929
Alyn Hilsden <=>
2025-07-22 18:02:59 +0100

    Customizing the logging so that stacktrace is logged in separate verbose file

--- config/logging.php
+++ config/logging.php
@@ -54,17 +54,22 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => explode(',', (string) env('LOG_STACK', 'single')),
+            'channels' => ['single', 'verbose'],
             'ignore_exceptions' => false,
         ],
-
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/singleLine.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
+            'tap' => [App\Logging\CustomizeFormatter::class],
         ],
-
+        'verbose' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/verbose.log'),
+            'level' => env('LOG_LEVEL', 'debug', 14),
+            'replace_placeholders' => true,
+        ],
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
@@ -89,7 +94,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
